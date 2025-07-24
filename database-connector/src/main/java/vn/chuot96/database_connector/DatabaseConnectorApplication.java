3536fd35628017edd07e3da143235b833f752c63
William Phan <phnam230197@gmail.com>
2025-07-24 15:18:29 +0700

    initial commit

---
+++ database-connector/src/main/java/vn/chuot96/database_connector/DatabaseConnectorApplication.java
@@ -0,0 +1,13 @@
+package vn.chuot96.database_connector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseConnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseConnectorApplication.class, args);
+	}
+
+}
---
+++ database-connector/src/test/java/vn/chuot96/database_connector/DatabaseConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.chuot96.database_connector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseConnectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
