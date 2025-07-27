36b9b7f7d866153fff778e10b7ad3eba75045b8c
William Phan <phnam230197@gmail.com>
2025-07-27 15:02:36 +0700

    repo add module authen-api

---
+++ authen-api/src/main/java/vn/chuot96/authenAPI/GatewayApiApplication.java
@@ -0,0 +1,13 @@
+package vn.chuot96.authenAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApiApplication.class, args);
+	}
+
+}
---
+++ authen-api/src/test/java/vn/chuot96/authenAPI/GatewayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.chuot96.authenAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
