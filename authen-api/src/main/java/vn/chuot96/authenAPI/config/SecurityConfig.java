b69d020af877c49ecf8a25e77898a1d4ec2e6da8
William Phan <phnam230197@gmail.com>
2025-07-27 23:25:26 +0700

    repo add module client-gateway-api

---
+++ authen-api/src/main/java/vn/chuot96/authenAPI/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package vn.chuot96.authenAPI.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain filterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/auth/**").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt
+                                .jwtDecoder(jwtDecoder())
+                        )
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public ReactiveJwtDecoder jwtDecoder() {
+        return NimbusReactiveJwtDecoder
+                .withJwkSetUri("http://localhost:8080/auth/oauth2/jwks")
+                .build();
+    }
+}
---
+++ client-gateway-api/src/main/java/vn/chuot96/clientGatewayAPI/ClientGatewayApplication.java
@@ -0,0 +1,13 @@
+package vn.chuot96.clientGatewayAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientGatewayApplication.class, args);
+	}
+
+}
