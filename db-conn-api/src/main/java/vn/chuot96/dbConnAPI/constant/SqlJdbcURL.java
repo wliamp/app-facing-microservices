3db2b6ad97fdc06f022f641cc4d352b0a7a75e54
William Phan <phnam230197@gmail.com>
2025-07-27 13:03:08 +0700

    module db-conn-api rename SqlTye.java class modify SqlService.java class remove SqlConverter.java class clarity classes, variables

---
+++ db-conn-api/src/main/java/vn/chuot96/dbConnAPI/constant/SqlJdbcURL.java
@@ -0,0 +1,30 @@
+package vn.chuot96.dbConnAPI.constant;
+
+import lombok.Getter;
+import org.springframework.web.bind.annotation.RequestBody;
+import vn.chuot96.dbConnAPI.dto.SqlRequestDTO;
+
+@Getter
+public enum SqlJdbcURL {
+
+    MYSQL("com.mysql.cj.jdbc.Driver", "jdbc:mysql://%s:%s/%s"),
+
+    POSTGRES("org.postgresql.Driver", "jdbc:postgresql://%s:%s/%s"),
+
+    MSSQL("com.microsoft.sqlserver.jdbc.SQLServerDriver", "jdbc:sqlserver://%s:%s;databaseName=%s");
+
+    // --> more SQL type here
+
+    private final String driverClass;
+    private final String pattern;
+
+    SqlJdbcURL(String driverClass, String pattern) {
+        this.driverClass = driverClass;
+        this.pattern = pattern;
+    }
+
+    public String setJdbcUrl(@RequestBody SqlRequestDTO request) {
+        return String.format(pattern, request.getHost(), request.getPort(), request.getDatabase());
+    }
+
+}
--- db-conn-api/src/main/java/vn/chuot96/dbConnAPI/service/SqlService.java
+++ db-conn-api/src/main/java/vn/chuot96/dbConnAPI/service/SqlService.java
@@ -3,23 +3,24 @@ package vn.chuot96.dbConnAPI.service;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
-import vn.chuot96.dbConnAPI.constant.SqlType;
 import vn.chuot96.dbConnAPI.dto.SqlRequestDTO;
 import vn.chuot96.dbConnAPI.util.SqlHandler;
+
+import static vn.chuot96.dbConnAPI.constant.SqlJdbcURL.*;
 
 @Service
 public class SqlService {
 
     public static ResponseEntity<?> mysqlQuery(@RequestBody SqlRequestDTO request) {
-        return SqlHandler.execute(SqlType.MYSQL.getDriverClass(), SqlType.MYSQL.getJdbcUrl(request), request);
+        return SqlHandler.execute(MYSQL.getDriverClass(), MYSQL.setJdbcUrl(request), request);
     }
 
     public static ResponseEntity<?> postgresQuery(@RequestBody SqlRequestDTO request) {
-        return SqlHandler.execute(SqlType.POSTGRES.getDriverClass(), SqlType.POSTGRES.getJdbcUrl(request), request);
+        return SqlHandler.execute(POSTGRES.getDriverClass(), POSTGRES.setJdbcUrl(request), request);
     }
 
     public static ResponseEntity<?> mssqlQuery(@RequestBody SqlRequestDTO request) {
-        return SqlHandler.execute(SqlType.MSSQL.getDriverClass(), SqlType.MSSQL.getJdbcUrl(request), request);
+        return SqlHandler.execute(MSSQL.getDriverClass(), MSSQL.setJdbcUrl(request), request);
     }
 
 }
