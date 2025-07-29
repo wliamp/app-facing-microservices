7506ba566bc51afefd64ccfa96055d15ab97122b
William Phan <phnam230197@gmail.com>
2025-07-29 07:23:01 +0700

    module client-gateway-api modify application.properties; build.gradle; ClientGatewayApplication.java, JwtAuthenConfig.java classes finished module

--- client-gateway-api/src/main/java/vn/chuot96/clientGatewayAPI/ClientGatewayApplication.java
+++ client-gateway-api/src/main/java/vn/chuot96/clientGatewayAPI/ClientGatewayApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ClientGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ClientGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ClientGatewayApplication.class, args);
+    }
 
 }
--- client-gateway-api/src/main/java/vn/chuot96/clientGatewayAPI/config/JwtAuthenConfig.java
+++ client-gateway-api/src/main/java/vn/chuot96/clientGatewayAPI/config/JwtAuthenConfig.java
@@ -2,36 +2,25 @@ package vn.chuot96.clientGatewayAPI.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
-import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebFluxSecurity
-public class SecurityConfig {
-
+public class JwtAuthenConfig {
     @Bean
-    public SecurityWebFilterChain filterChain(ServerHttpSecurity http) {
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         http
                 .authorizeExchange(exchanges -> exchanges
-                        .pathMatchers("/auth/**").permitAll()
+                        .pathMatchers("/public/**").permitAll()
                         .anyExchange().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(jwt -> jwt
-                                .jwtDecoder(jwtDecoder())
-                        )
+                        .jwt(Customizer.withDefaults())
                 );
+
         return http.build();
     }
-
-    @Bean
-    public ReactiveJwtDecoder jwtDecoder() {
-        return NimbusReactiveJwtDecoder
-                .withJwkSetUri("http://localhost:8080/auth/oauth2/jwks")
-                .build();
-    }
-
 }
