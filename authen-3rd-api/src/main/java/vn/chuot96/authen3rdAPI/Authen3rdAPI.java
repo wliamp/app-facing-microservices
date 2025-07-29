62f09115f22ce409cf608db51344e3a93f1f4364
William Phan <phnam230197@gmail.com>
2025-07-30 03:45:37 +0700

    repo add module authen-3rd-api

---
+++ authen-3rd-api/src/main/java/vn/chuot96/authen3rdAPI/Authen3rdAPI.java
@@ -0,0 +1,13 @@
+package vn.chuot96.authen3rdAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Authen3rdAPI {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Authen3rdAPI.class, args);
+	}
+
+}
