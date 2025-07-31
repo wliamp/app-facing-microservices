97f9dccdd3245caf73b55106b5ae39e70e941bbe
William Phan <phnam230197@gmail.com>
2025-08-01 04:31:35 +0700

    Dev/3rd auth (#21)
    module authen-3rd-api
    add dto/UserDTO.java class
    modify OauthHandler.java class

--- authen-3rd-api/src/main/java/vn/chuot96/authen3rdAPI/util/OauthHandler.java
+++ authen-3rd-api/src/main/java/vn/chuot96/authen3rdAPI/util/OauthHandler.java
@@ -6,60 +6,36 @@ import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.web.client.RestTemplate;
-import vn.chuot96.authen3rdAPI.dto.ResponseDTO;
+import vn.chuot96.authen3rdAPI.dto.UserDTO;
 import vn.chuot96.authen3rdAPI.exception.InvalidTokenException;
 
-import java.util.Map;
-
-import static vn.chuot96.authen3rdAPI.constant.AuthMessage.INVALID_FACEBOOK_TOKEN;
-import static vn.chuot96.authen3rdAPI.constant.AuthMessage.INVALID_GOOGLE_TOKEN;
+import static vn.chuot96.authen3rdAPI.constant.AuthMessage.*;
 import static vn.chuot96.authen3rdAPI.constant.AuthProvider.*;
 
 public class OauthHandler {
 
     private static final RestTemplate restTemplate = new RestTemplate();
 
-    public static ResponseDTO googleProvider(String token) {
+    public static UserDTO googleProvider(String token) {
         try {
             JwtDecoder decoder = NimbusJwtDecoder.withJwkSetUri(GOOGLE.getEndpoint()).build();
             Jwt jwt = decoder.decode(token);
-
-            ObjectMapper mapper = new ObjectMapper();
-            Map<String, Object> info = Map.of(
-                    "email", jwt.getClaimAsString("email"),
-                    "name", jwt.getClaimAsString("name"),
-                    "picture", jwt.getClaimAsString("picture"),
-                    "sub", jwt.getSubject()
-            );
-
-            String rawJson = mapper.writeValueAsString(info);
-            return new ResponseDTO(GOOGLE.name().toLowerCase(), rawJson);
+            return new UserDTO(GOOGLE.getKey(), jwt.getSubject());
         } catch (Exception e) {
             throw new InvalidTokenException(INVALID_GOOGLE_TOKEN.getMessage(), e);
         }
     }
 
-    public static ResponseDTO facebookProvider(String token) {
+    public static UserDTO facebookProvider(String token) {
         try {
             String url = FACEBOOK.getEndpoint() + token;
             String response = restTemplate.getForObject(url, String.class);
-
             ObjectMapper mapper = new ObjectMapper();
             JsonNode json = mapper.readTree(response);
-
-            Map<String, Object> info = Map.of(
-                    "email", json.path("email").asText(null),
-                    "name", json.path("name").asText(null),
-                    "picture", json.path("picture").path("data").path("url").asText(null),
-                    "id", json.path("id").asText()
-            );
-
-            String rawJson = mapper.writeValueAsString(info);
-            return new ResponseDTO(FACEBOOK.name().toLowerCase(), rawJson);
+            return new UserDTO(FACEBOOK.getKey(), json.path("id").asText());
         } catch (Exception e) {
             throw new InvalidTokenException(INVALID_FACEBOOK_TOKEN.getMessage(), e);
         }
     }
-
 
 }
