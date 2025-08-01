02bc1a0414a4422f0b50a24e84447e6f43854cf6
William Phan <phnam230197@gmail.com>
2025-08-01 19:53:50 +0700

    module authen-3rd-api

--- authen-3rd-api/src/main/java/vn/chuot96/auth3rdapi/component/OtpProvider.java
+++ authen-3rd-api/src/main/java/vn/chuot96/auth3rdapi/component/OtpProvider.java
@@ -1,8 +1,8 @@
 package vn.chuot96.auth3rdapi.component;
 
-import vn.chuot96.auth3rdapi.dto.UserDTO;
+import vn.chuot96.auth3rdapi.dto.TokenResponseDTO;
 
 public interface OtpProvider {
     boolean supports(String token);
-    UserDTO verifyToken(String token);
+    TokenResponseDTO verifyToken(String token);
 }
