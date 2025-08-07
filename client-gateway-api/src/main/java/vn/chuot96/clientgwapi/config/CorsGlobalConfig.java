a639e3a82f25d31e03ed477e4b204f6a733b7b2d
William Phan <phnam230197@gmail.com>
2025-08-07 07:28:18 +0700

    module client-gateway-api
    add application-gate1.properties\nmodify application.properties\nmodify all classes

--- client-gateway-api/src/main/java/vn/chuot96/clientgwapi/config/CorsGlobalConfig.java
+++ client-gateway-api/src/main/java/vn/chuot96/clientgwapi/config/CorsGlobalConfig.java
@@ -1,17 +1,24 @@
 package vn.chuot96.clientgwapi.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 
+@Slf4j
 @Configuration
-public class CorsGlobalConfig implements WebFluxConfigurer{
-        @Override
-        public void addCorsMappings(CorsRegistry registry) {
-            registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:3000", "http://127.0.0.1:5500")  // frontend domains
-                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                    .allowedHeaders("*")
-                    .allowCredentials(true);
-        }
+public class CorsGlobalConfig implements WebFluxConfigurer {
+    @Value("${cors.allowed-origins}")
+    private String[] allowedOrigins;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+        log.info("CORS configuration applied. Allowed origins: {}", (Object) allowedOrigins);
+    }
 }
