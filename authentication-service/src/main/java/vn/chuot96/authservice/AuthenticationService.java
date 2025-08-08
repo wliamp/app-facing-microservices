5858739a6befd3c7c80e4ec9b27b9035e70c8ee0
William Phan <phnam230197@gmail.com>
2025-08-08 23:45:08 +0700

    repo
    add module authentication-service\nmigrate from other own repo

---
+++ authentication-service/src/main/java/vn/chuot96/authservice/AuthenticationService.java
@@ -0,0 +1,11 @@
+package vn.chuot96.authservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationService {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthenticationService.class, args);
+    }
+}
---
+++ authentication-service/src/test/java/vn/chuot96/authservice/AuthenticationServiceTests.java
@@ -0,0 +1,11 @@
+package vn.chuot96.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationServiceTests {
+
+    @Test
+    void contextLoads() {}
+}
