1479221f288e26531b17dc1c85697d2f60eca0f4
William Phan <phnam230197@gmail.com>
2025-08-08 10:51:31 +0700

    repo
    remove module database-connector-api
    
    migrated the module to other repo

--- database-connector-api/src/main/java/vn/chuot96/dbconnapi/dto/NosqlRequestDTO.java
+++
@@ -1,6 +0,0 @@
-package vn.chuot96.dbconnapi.dto;
-
-import java.util.Map;
-
-public record NosqlRequestDTO(
-        DbRequestDTO db, String collection, Map<String, Object> data, Map<String, Object> filter) {}
--- database-connector-api/src/main/java/vn/chuot96/dbconnapi/dto/SqlRequestDTO.java
+++
@@ -1,5 +0,0 @@
-package vn.chuot96.dbconnapi.dto;
-
-import java.util.List;
-
-public record SqlRequestDTO(DbRequestDTO db, String query, List<Object> params) {}
