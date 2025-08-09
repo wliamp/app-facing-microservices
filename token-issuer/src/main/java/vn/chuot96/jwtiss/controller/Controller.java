581006c041508d1700dd5c9fbdd16a01ee4bd480
William Phan <phnam230197@gmail.com>
2025-08-09 13:20:21 +0700

    module token-issuer
    add RedisHelper, RedisConfg classes\nmodify almost files

--- token-issuer/src/main/java/vn/chuot96/jwtiss/controller/Controller.java
+++ token-issuer/src/main/java/vn/chuot96/jwtiss/controller/Controller.java
@@ -3,24 +3,22 @@ package vn.chuot96.jwtiss.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import vn.chuot96.jwtiss.dto.RequestDTO;
-import vn.chuot96.jwtiss.dto.ResponseDTO;
-import vn.chuot96.jwtiss.service.TokenService;
+import vn.chuot96.jwtiss.dto.Request;
+import vn.chuot96.jwtiss.service.IssueService;
 
 @RestController
 @RequestMapping("/issue")
 @RequiredArgsConstructor
 public class Controller {
-    private final TokenService tokenservice;
+    private final IssueService issueService;
 
-    @PostMapping("/access-refresh")
-    public ResponseEntity<?> issue(@RequestBody RequestDTO request) {
-        return ResponseEntity.ok(
-                new ResponseDTO(tokenservice.issueAccess(request), tokenservice.issueRefresh(request)));
+    @PostMapping("/non-refresh")
+    public ResponseEntity<?> issue(@RequestBody Request request) {
+        return ResponseEntity.ok(issueService.firstLogin(request));
     }
 
-    @PostMapping("/access")
-    public ResponseEntity<?> issueAccess(@RequestBody RequestDTO request) {
-        return ResponseEntity.ok(tokenservice.issueAccess(request));
+    @PostMapping("/with-refresh")
+    public ResponseEntity<?> issueAccess(@RequestBody String refresh) {
+        return ResponseEntity.ok(issueService.rememberLogin(refresh));
     }
 }
