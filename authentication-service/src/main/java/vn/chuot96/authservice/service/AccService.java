08020585bab8f289fbfdc8420040c2f3154a729a
William Phan <phnam230197@gmail.com>
2025-08-09 11:14:56 +0700

    module authentication-service
    add RedisHelper, RedisConfig classes\nmodify ForwardHelper, Controller, UserInfo, Acc, AccService, AuthService, ForwardService, Generator classes\n modify application-auth1.properties, build.gradle

--- authentication-service/src/main/java/vn/chuot96/authservice/service/AccService.java
+++ authentication-service/src/main/java/vn/chuot96/authservice/service/AccService.java
@@ -1,5 +1,6 @@
 package vn.chuot96.authservice.service;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
@@ -16,20 +17,20 @@ public class AccService {
         return accRepo.findIdByCredential(credential);
     }
 
-    public Mono<Long> insertGuestAcc(String credential) {
+    public Mono<Acc> insertGuestAcc() {
         return accRepo.save(Acc.builder()
-                        .code(Generator.generateCode())
-                        .credential(credential)
-                        .build())
-                .map(Acc::getId);
+                .code(Generator.generateCode(8))
+                .credential("guest" + Generator.generateCode(32))
+                .scope("profile:read profile:write token:get")
+                .audiences(List.of("authentication-service", "profile-service", "token-issuer", "third-party-verify"))
+                .build());
     }
 
-    public Mono<Long> updateCredential(String current, String replace) {
+    public Mono<Acc> updateCredential(String current, String replace) {
         return accRepo.findByCredential(current)
                 .flatMap(acc -> {
                     acc.setCredential(replace);
                     return accRepo.save(acc);
-                })
-                .map(Acc::getId);
+                });
     }
 }
